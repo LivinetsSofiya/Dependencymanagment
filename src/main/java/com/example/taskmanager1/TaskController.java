46cb83715c6c77f8090180bf28148adcf65b59d3
SLYVYNET <sofiialyvynets@otto.de>
2023-08-30 09:17:51 +0200

    Workable version of Task app

--- src/main/java/com/example/taskmanager1/TaskController.java
+++ src/main/java/com/example/taskmanager1/TaskController.java
@@ -14,21 +14,20 @@ public class TaskController {
     private TaskRepository taskRepository;
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(TaskController.class);
 
-    /* @GetMapping
+    @GetMapping
     public String listTasks(Model model) {
         model.addAttribute("tasks", taskRepository.findAll());
-        model.addAttribute("newTask", new Task()); // For the form
+        model.addAttribute("task", new Task()); // For the form
         return "index";
     }
-*/
-    @PostMapping("add")
+@PostMapping("add")
     public String addTask(@ModelAttribute Task task) {
         log.info("add Task");
         taskRepository.save(task);
         return "redirect:/";
     }
 
-  /*  @GetMapping("edit/{id}")
+   @GetMapping("edit/{id}")
     public String editTask(@PathVariable Long id, Model model) {
         Task task = taskRepository.findById(id).orElse(null);
         if (task != null) {
@@ -47,5 +46,5 @@ public class TaskController {
     public String deleteTask(@PathVariable Long id) {
         taskRepository.deleteById(id);
         return "redirect:/";
-    }*/
+    }
 }
