0e5b4208ba0928328e80d507188cfe0fe22f01b8
SLYVYNET <sofiialyvynets@otto.de>
2023-08-30 11:12:41 +0200

    Test for adding method

--- src/test/java/com/example/taskmanager1/TaskManager1ApplicationTests.java
+++ src/test/java/com/example/taskmanager1/TaskManager1ApplicationTests.java
@@ -2,12 +2,31 @@ package com.example.taskmanager1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class TaskManager1ApplicationTests {
-
+    @Autowired
+    private TaskRepository taskRepository;
     @Test
-    void contextLoads() {
-    }
+    public void testAddTask() {
 
+        Task task = new Task();
+        task.setTitle("Test Task");
+        task.setDescription("Test Description");
+        task.setCompleted(false);
+
+
+        taskRepository.save(task);
+
+
+        Task savedTask = taskRepository.findById(task.getId()).orElse(null);
+        assertNotNull(savedTask);
+        assertEquals("Test Task", savedTask.getTitle());
+        assertEquals("Test Description", savedTask.getDescription());
+        assertFalse(savedTask.isCompleted());
+    }
 }
